2535fe898e9e5de28d623395ee2ca442198cc816
NicoMaldos <45697877+NicoMaldos@users.noreply.github.com>
2019-09-21 14:42:07 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<dirent.h>
+#include"file_manage.h"
+//#include"polinomio.h"
+//int argc, char *argv[]
+
+int main(){
+    
+    remove("mazo/2_amarillo.txt");
+
+
+    return 0;
+}
